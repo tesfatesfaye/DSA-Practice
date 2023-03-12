1d9a956e0295917c7cd732aa344e9df22412271f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-03-12 12:57:47 -0400

    added new function same js file

--- same.js
+++ same.js
@@ -1,4 +1,5 @@
-function sim(arrayOne, arrayTwo){
+//function that checks if the values of one array squared in another, they must have the same frequency
+function squared(arrayOne, arrayTwo){ //example one 
     let similar=[]
     if(arrayOne.length!==arrayTwo.length){
          return false
@@ -26,7 +27,29 @@ function sim(arrayOne, arrayTwo){
 
 }
 
-console.log(sim([2,4,8],[4,16,78]))
-console.log(sim([1],[1]))
+console.log(squared([2,4,8],[4,16,64]))
+console.log(squared([1],[1]))
+
+// example one works fine but a cleaner version is presented below.
 
 
+function squaredTwo(arrayOne,arrayTwo){ //very similar solution albeit cleaner however, it's still o(n^2)
+    if(arrayOne.length!==arrayTwo.length){
+            return false
+    }
+for(let i=0; i<arrayOne.length;i++){
+    let holder=arrayTwo.indexOf(arrayOne[i] ** 2)
+    if(holder===-1){
+        return false
+    }
+    console.log(arrayTwo)
+    arrayTwo.splice(holder,1)
+
+}
+        return true;
+
+}
+
+console.log(squaredTwo([2,4,8],[4,16,64]))
+console.log(squaredTwo([2,4,8],[4,16,12]))
+console.log(squaredTwo([2,4,8],[4,16]))
