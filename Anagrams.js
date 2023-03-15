cf90cb6ecd074b84eccc0102e0c7359c3c68601f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-03-15 15:31:23 -0400

    rewrote anagram and made it cleaner

--- Anagrams.js
+++ Anagrams.js
@@ -8,13 +8,15 @@ let stringTwoHolder={}
 
 for(let i of stringOne){
 
-    stringOneHolder[i] ? stringOneHolder[i]=+1 : stringOneHolder[i]=1
+    stringOneHolder[i] ? stringOneHolder[i]+=1 : stringOneHolder[i]=1
 }
 
 for(let i of stringTwo){
 
-    stringTwoHolder[i] ? stringTwoHolder[i]=+ 1 : stringTwoHolder[i]=1
+    stringTwoHolder[i] ? stringTwoHolder[i]+= 1 : stringTwoHolder[i]=1
 }
+
+
 
 for(let i in stringOneHolder){
     if(!( i in stringTwoHolder)){
@@ -25,9 +27,42 @@ for(let i in stringOneHolder){
         return false
     }
 }
+
+
 return true
 
 
 }
 
 console.log(anagrams("hello","ollhe"))
+
+
+// Rewrote the function and made it cleaner
+
+function anagramsTwo(stringOne,stringTwo){
+    if(stringOne.length!==stringTwo.length){
+        return false
+    }
+    
+    let stringOneHolder={}
+  
+    
+    for(let i of stringOne){
+    
+        stringOneHolder[i] ? stringOneHolder[i]+=1 : stringOneHolder[i]=1
+    }
+    
+    for(let i of stringTwo){
+        if(!(stringOneHolder[i])){
+            return false;
+        }
+        stringOneHolder[i] -=1
+    }
+    
+ 
+    return true
+    
+
+}
+
+console.log(anagramsTwo("",""))
