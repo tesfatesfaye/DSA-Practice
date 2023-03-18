8ac2a2610aa1ff406bd4fd24ac87a0b1c27668ba
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-03-18 14:14:56 -0400

    made changes to the first anagram function

--- Problem Solving patterns/Frequency counters/Anagrams.js
+++ Problem Solving patterns/Frequency counters/Anagrams.js
@@ -1,38 +1,30 @@
-function anagrams(stringOne, stringTwo){
-if(stringOne.length!==stringTwo.length){
-    return false
-}
-
-let stringOneHolder={}
-let stringTwoHolder={}
-
-for(let i of stringOne){
-
-    stringOneHolder[i] ? stringOneHolder[i]+=1 : stringOneHolder[i]=1
-}
-
-for(let i of stringTwo){
-
-    stringTwoHolder[i] ? stringTwoHolder[i]+= 1 : stringTwoHolder[i]=1
-}
-
-
-
-for(let i in stringOneHolder){
-    if(!( i in stringTwoHolder)){
+function validAnagram(stringOne,stringTwo){
+    if(stringTwo.length!==stringOne.length){
         return false
     }
-
-    if(stringOneHolder[i]!==stringTwoHolder[i]){
-        return false
+    let stringOneHolder={}
+    let stringTwoHolder={}
+    
+    for(let value of stringOne){
+        stringOneHolder[value] ? stringOneHolder[value]++ : stringOneHolder[value]=1 
     }
-}
-
-
-return true
-
-
-}
+    
+      for(let value of stringTwo){
+        stringTwoHolder[value] ? stringTwoHolder[value]++ : stringTwoHolder[value]=1 
+    }
+    
+    for(let value in stringOneHolder){
+        if(!(stringTwoHolder[value])){
+            return false
+        }
+        
+        if(stringOneHolder[value]!==stringTwoHolder[value]){
+            return false
+        }
+        
+    }
+    return true
+  }
 
 console.log(anagrams("hello","ollhe"))
 
