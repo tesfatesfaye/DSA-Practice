9a9577628a5888ef832283d824a2761aab10c554
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-03-23 12:51:41 -0400

    added a more preformative version of the maxsubarry function

--- Problem Solving patterns/Sliding Window Pattern/slidingWindow.js
+++ Problem Solving patterns/Sliding Window Pattern/slidingWindow.js
@@ -20,3 +20,26 @@ function maxSubarraySum(arrayInput,consecutive){
 
 console.log(maxSubarraySum([-1,-2,-3,-10,-91,-34],2))
 
+function maxSubarraySumTwo(arrayInput,value){
+    if(arrayInput.length===0){
+        return null
+    }
+    else if(value>=arrayInput.length){
+        return arrayInput.reduce((i,j)=>i+j)
+    }
+    let maxValue=0
+    let tempValue=0
+
+    for(let i=0;i<value;i++){
+        maxValue+=arrayInput[i]
+    }
+   tempValue=maxValue
+
+    for(let i=value;i<arrayInput.length;i++){
+        tempValue=tempValue-arrayInput[i-value]+arrayInput[i]
+        maxValue=Math.max(tempValue,maxValue)
+    }
+    return maxValue
+}
+
+console.log(maxSubarraySumTwo([-1,-2,-3,-10,91,34],2))
