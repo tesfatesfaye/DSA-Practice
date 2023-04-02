11b172b2f6212dae63e63f6228f3c226ebf68993
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-02 19:44:17 -0400

    added new function using pointers for areThereDuplicate

--- Problem Solving patterns/Multiple Pointers Pattern/areThereDuplicates.js
+++ Problem Solving patterns/Multiple Pointers Pattern/areThereDuplicates.js
@@ -15,3 +15,26 @@ for(let value of inputs){
 }
 
 console.log(areThereDuplicates(1,2,3,2))
+
+const areThereDuplicatesPointers=(...inputs)=>{
+       
+    let leftPointer=0
+    let rightPointer=1
+    while(leftPointer<inputs.length-1){
+        if(rightPointer<inputs.length){
+            if(inputs[rightPointer]===inputs[leftPointer]){
+                return true
+            }
+            else{
+                rightPointer++
+            }
+        }
+        else{
+            leftPointer++
+            rightPointer=leftPointer+1
+        }
+    }
+    return false
+
+}
+console.log(areThereDuplicatesPointers(1,2,3,1))
