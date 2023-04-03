4db61f3c8766be315bcf007cbdc0495ea98d2a89
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-03 12:52:34 -0400

    created new js file for the are there duplicates function for when it's being utilized by way of frequency counters

--- Problem Solving patterns/Multiple Pointers Pattern/areThereDuplicates.js
+++ Problem Solving patterns/Multiple Pointers Pattern/areThereDuplicates.js
@@ -1,21 +1,3 @@
-const areThereDuplicates=(...inputs)=>{// frequency counter pattern
-const inputHolder={}
-
-for(let value of inputs){
-    if(!inputHolder[value]){
-        inputHolder[value]=1
-    }
-    else {
-        return true
-    }
-}
-    return false
-
-
-}
-
-console.log(areThereDuplicates(1,2,3,2))
-
 const areThereDuplicatesPointers=(...inputs)=>{
        
     let leftPointer=0
