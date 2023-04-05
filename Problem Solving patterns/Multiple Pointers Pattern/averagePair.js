090d87f65baf0dff75123065a5f0993f8a5f63ee
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-05 13:56:05 -0400

    implemented a more efficent version of the average pair function

--- Problem Solving patterns/Multiple Pointers Pattern/averagePair.js
+++ Problem Solving patterns/Multiple Pointers Pattern/averagePair.js
@@ -20,3 +20,27 @@ let rightPointer=1
 }
 
 console.log(averagePair([1,2,3],2.5))
+
+
+const averagePairTwo=(arr,value)=>{
+if(arr.length<2) return false
+let leftPointer=0
+let rightPointer=arr.length-1
+
+while(leftPointer<rightPointer){
+    if((arr[leftPointer]+arr[rightPointer])/2>value){
+        rightPointer--
+    }
+    else if((arr[leftPointer]+arr[rightPointer])/2<value){
+        leftPointer++
+    }
+    else{
+        return true
+    }
+
+}
+    return false
+
+}
+
+console.log(averagePairTwo([1,2,9],2.5))
