3e4aace66a56b2c7072431086ee071d4aaa05fa9
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-08 17:27:50 -0400

    fixed issue with findlongestsubstring function

--- Problem Solving patterns/findLongestSubstring.js
+++ Problem Solving patterns/findLongestSubstring.js
@@ -1,10 +1,11 @@
-const findLongestSubstring=(str)=>{
+const findLongestSubstring=(str)=>{ // uses the sliding window patter and the frequency counter pattern
 if(str.length===0) return 0
-let mainStr=""
-let temp=""
 let start=0
-let place=0
+let temp=str[start]
+let mainStr=temp
+let place=1
 let strHolder={}
+strHolder[str[start]]=1
 while(place<str.length){
 if(!strHolder[str[place]]){
     strHolder[str[place]]=1
@@ -15,18 +16,20 @@ if(!strHolder[str[place]]){
     place++
 }
 else{
-    temp=str[place]
+    start++
+    place=start+1
+    temp=str[start]
     strHolder={}
-    strHolder[str[place]]=1
-    place++
+    strHolder[str[start]]=1
+   
 }
 
 
 }
 
-return mainStr
+return mainStr.length
 
 }
 
-console.log(findLongestSubstring("thisisawesome"))
+console.log(findLongestSubstring("thisishowwedoit"))
 
