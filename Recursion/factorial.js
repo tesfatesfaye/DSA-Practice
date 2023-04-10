c9bc1529c322e75519c334ef91e42b9b7bf7345f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-10 13:45:49 -0400

    added an else if statment to the factorial Recursive function as input validation

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -13,7 +13,9 @@ const factorialRecursive=(num)=>{
     if(num===1){
         return 1
     }
+    else if(num>1){
  return num * factorialRecursive(num-1)
+    }
 }
 
 console.log(factorialRecursive(4))
