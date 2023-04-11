e984de9359e2e1721abe0583f21147960733bcee
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-11 18:39:21 -0400

    optimized the fib function

--- Recursion/problemSetOne/fib.js
+++ Recursion/problemSetOne/fib.js
@@ -1,19 +1,31 @@
-const fib=(input)=>{
-    if(input===1||input===2) return 1
-    let fibValues=[1,1]
-        const helperFunction=(inputIndex)=>{
+// const fib=(input)=>{
+//     if(input<=2) return 1
+//     let fibValues=[1,1]
+//         const helperFunction=(inputIndex)=>{
         
-        if(fibValues.length===inputIndex)return 
+//         if(fibValues.length===inputIndex)return 
             
-        fibValues.push(fibValues[fibValues.length-1]+fibValues[fibValues.length-2])
+//         fibValues.push(fibValues[fibValues.length-1]+fibValues[fibValues.length-2])
         
-        helperFunction(inputIndex)
-        }
+//         helperFunction(inputIndex)
+//         }
 
-        helperFunction(input)
+//         helperFunction(input)
 
-        return fibValues[input-1]
+//         return fibValues[input-1]
 
-}
+// }
 
-console.log(fib(8))
+// console.log(fib(1000))
+
+
+
+const fibTwo=(n)=>{
+    if(n===0|| n===1) return n
+    
+    return fib(n-1)+fib(n-2)
+    
+    
+    }
+    
+    console.log(fibTwo(1000))
