f198bdeca4bd71e70feacf7017147de61acacc29
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-12 17:34:46 -0400

    added a second way of doiing the flatten function using a helper method

--- Recursion/problemsetTwo/flatten.js
+++ Recursion/problemsetTwo/flatten.js
@@ -19,3 +19,27 @@ let flatArray=[]
 
 }
 
+const flattenHelper=(arr)=>{
+let flatArray=[]
+
+const helper=(arr)=>{
+    for(let i of arr){
+        if(Array.isArray(i)){
+            helper(i)
+        }
+
+        else {
+            flatArray.push(i)
+        }
+    
+    
+    }
+
+    return flatArray
+}
+    helper(arr)
+    return flatArray
+
+}
+
+console.log(flattenHelper([1,2,3,4,[5,[6]],9]))
