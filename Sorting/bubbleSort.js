9498d6d307086e24584d77daba9cdccb2cb16edf
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-19 20:13:10 -0400

    set up simple bubble sort function

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,13 +1,27 @@
-function swap(i,j){
-let temp=i
-i=j
-j=temp
-
-}
-
-
-
 const bubbleSort=(arr)=>{
-
+    let temp=0
+    let counter=0
+    let newArray=[]
+    for(let i=0;i<arr.length;i++){
+       for(let j=0;j<arr.length-i;j++){
+        counter++
+        console.log(arr)
+        if(arr[j]>arr[j+1]){
+             temp=arr[j+1]
+           arr[j+1]=arr[j]
+            arr[j]=temp
+        }
+       
+       } 
+       
+    }
+    console.log(counter)
+    return arr
 
 }
+    console.log(bubbleSort([1,2,5,4,3,19,12,11]))
+
+
+
+
+
