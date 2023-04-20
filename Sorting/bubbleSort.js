660833b9fc7a8f20a65352030373fa840bcac574
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-19 20:25:22 -0400

    added instructer solution to bubble sort

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,14 +1,10 @@
 const bubbleSort=(arr)=>{
-    let temp=0
-    let counter=0
+       let counter=0
       for(let i=0;i<arr.length;i++){
        for(let j=0;j<arr.length-i;j++){
         counter++
-        console.log(arr)
-        if(arr[j]>arr[j+1]){
-             temp=arr[j+1]
-           arr[j+1]=arr[j]
-            arr[j]=temp
+         if(arr[j]>arr[j+1]){
+           [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
         }
        
        } 
@@ -18,9 +14,30 @@ const bubbleSort=(arr)=>{
     return arr
 
 }
-    console.log(bubbleSort([1,2,5,4,3,19,12,11]))
+    console.log(bubbleSort([1,2,5,4,-3,9,12]))
 
 
 
 
+const bubbleSortColt=(arr)=>{
+    let temp=0
+    let counter=0
+    for(let i=arr.length;i>0;i--){
+        for(let j=0;j<i;j++){
+            counter++
+            if(arr[j]>arr[j+1]){
+             temp=arr[j+1]
+           arr[j+1]=arr[j]
+            arr[j]=temp
+        }
 
+    }
+
+
+}
+        console.log(counter)
+        return arr
+
+}
+
+console.log(bubbleSortColt([1,2,5,4,-3,9,12]))
