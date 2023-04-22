db4f851d76f68084cc899bc721e87eafd6d41f87
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-21 20:27:53 -0400

    implemented colts solution

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -14,3 +14,23 @@ const insertionSort=(arr)=>{ //my solution
 }
 
 console.log(insertionSort([4,5,6,1,2,19]))
+
+const insertionSortTwo=(arr)=>{ // less obvious answer but it works
+    
+    for(let i=1;i<arr.length;i++){
+        let position=i
+        let value=arr[i]
+        for(let j=i-1;j>=0 && arr[j]>value;j--){
+            arr[j+1]=arr[j]
+            position=j
+        
+        }
+        arr[position]=value
+        
+        
+    }
+    return arr
+    
+}
+
+console.log(insertionSortTwo([4,5,6,1,2,19]))
