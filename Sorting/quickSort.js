a40aae39a2a938d140696f194936fb4065049b3f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-24 20:47:37 -0400

    implemented the helper method needed for the quick sort method

---
+++ Sorting/quickSort.js
@@ -0,0 +1,18 @@
+function pivotHelper(arr,start=0,end=arr.length+1){
+const swap=(arr,i,j)=>{
+        [arr[i],arr[j]]=[arr[j],arr[i]]
+    }
+let pivot=arr[start]
+let swapIdx=start
+ for(let i=start+1;i<arr.length;i++){
+   
+   if(pivot>arr[i]){
+    swapIdx++;
+     swap(arr,swapIdx,i)
+   }
+ }
+ swap(arr,start,swapIdx)
+ return arr
+}
+
+console.log(pivotHelper([4,8,2,1,5,7,6,3]))
