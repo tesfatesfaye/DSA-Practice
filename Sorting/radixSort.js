a48c0369ba5d747629cf24762c998195eddcaa8d
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-25 23:16:32 -0400

    implemented new getDigit helper function

--- Sorting/radixSort.js
+++ Sorting/radixSort.js
@@ -1,10 +1,16 @@
-const getDigit=(num,digit)=>{
-let stringNum=num.toString()
-let value=0
-digit<stringNum.length ? value=stringNum[stringNum.length-(digit)-1] : value=0
+// const getDigit=(num,digit)=>{
+// let stringNum=num.toString()
+// let value=0
+// digit<stringNum.length ? value=stringNum[stringNum.length-(digit)-1] : value=0
 
-return parseInt(value)
+// return parseInt(value)
+// }
+
+
+// console.log(getDigit(12345,2))
+
+const getDigit=(num,i)=>{
+    return Math.floor(Math.abs(num)/Math.pow(10,i))%10
 }
 
-
-console.log(getDigit(12345,2))
+ console.log(getDigit(12345,3))
