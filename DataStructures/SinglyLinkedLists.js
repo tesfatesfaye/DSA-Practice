2a037f9d3f650ee378d5618123413b084a7c0aec
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-29 15:39:22 -0400

    implemented pop function for singly linked list

--- DataStructures/SinglyLinkedLists.js
+++ DataStructures/SinglyLinkedLists.js
@@ -26,19 +26,41 @@ class SinglyLinkedList{
             this.tail=newNode
             this.length=this.length+1
         }
+       
             return this;
 
     }
 
+        pop(){
+                if(!this.head) return undefined
+                let current=this.head
+                let newTail=this.head
+                while(current.next){
+                    newTail=current
+                    current=current.next
+                }
+                   this.tail=newTail;
+                   this.tail.next=null
+                   this.length=this.length-1
+                   if(this.length===0){
+                    this.head=null
+                    this.tail=null
+                   }
+                    return current
+                }
+
+
 }
 
 let list=new SinglyLinkedList()
-// list.push("Hi")
-// console.log(list.push("GoodBye"))
-// console.log(list.push("how is your day"))
-
 list.push("Hi")
 list.push("You")
 list.push(99)
-console.log(list.head.next)
+list.push("sami")
+console.log(list.pop())
+console.log(list.pop())
+console.log(list.pop())
+console.log(list.pop())
 console.log(list)
+console.log(list.pop())
+
