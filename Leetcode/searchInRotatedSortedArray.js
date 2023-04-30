b81525bf54959a315d1f1fc782f11e271aca9073
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-04-30 18:59:54 -0400

    fixed issue with merge sort algo

--- Leetcode/searchInRotatedSortedArray.js
+++ Leetcode/searchInRotatedSortedArray.js
@@ -50,16 +50,15 @@ const search = (arrayValue,value) => {
         return 0
     }
 
-    let kValue=findK(arrayValue)
-    if(kValue>0){
-        let index=binarySearch(arrayValue,value,start=0,end=kValue-1)
-        if(index>-1){
-            return index
+        let kValue=findK(arrayValue)
+        if(kValue>0){
+            let index=binarySearch(arrayValue,value,0,kValue-1)
+            if(index>-1){
+                return index
+            }
         }
-    }
 
-    return(binarySearch(arrayValue,value,start=kValue,end=arrayValue.length-1))
+        return(binarySearch(arrayValue,value,kValue))
 
 }
-
 console.log(search([8,9,10,11,12,1,2,3,4,6,7],4))
