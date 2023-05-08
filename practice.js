d60efa4ed11123455a5fd8658391fc5a67ac3404
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-08 18:37:15 -0400

    commented out nodetester

--- practice.js
+++ practice.js
@@ -13,9 +13,27 @@ class DoublyLinkedLists{
         this.length=0
 
     }
+    push(value){
+        if(!this.tail){
+            let pushedValue=new Node(value)
+            this.head=pushedValue
+            this.tail=pushedValue
+            this.length=1
+            
+        }
+        else {
+            let pushedValue=new Node(value)
+            this.tail.next=pushedValue
+            pushedValue.prev=this.tail
+            this.tail=pushedValue
+            
+
+        }
+        return this
+    }
 }
 
-const NodeTester=new Node(13)
-NodeTester.next=new Node(14)
-NodeTester.next.prev=NodeTester
-console.log(NodeTester.next.prev.val)
+// const NodeTester=new Node(13)
+// NodeTester.next=new Node(14)
+// NodeTester.next.prev=NodeTester
+// console.log(NodeTester.next.prev.val)
