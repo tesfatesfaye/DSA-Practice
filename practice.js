0da63d49bbd8c7815dafd926f87ad3a17915cd66
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-21 03:40:02 -0400

    added a second implementation

--- practice.js
+++ practice.js
@@ -12,6 +12,18 @@ queuesArray.shift()
 queuesArray.shift()
 queuesArray.shift()
 
+const queuesArrayTwo=[]
+// we do the same thing the other way around
+queuesArray.unshift(0)
+queuesArray.unshift(1)
+queuesArray.unshift(2)
+queuesArray.unshift(3)
+// removing the inserted values
+queuesArray.pop()
+queuesArray.pop()
+queuesArray.pop()
+queuesArray.pop()
+
 
 
 
