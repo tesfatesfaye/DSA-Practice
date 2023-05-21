edb1a3f58bacab2529239e95769946044fd4e779
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-21 03:53:00 -0400

    implemnted queue DS in a similar fashion to a singly linked list

--- practice.js
+++ practice.js
@@ -11,7 +11,43 @@ class Queue{ //since push and pop have to be O(1) time we will use shift and uns
         this.last=null;
         this.size=0;
     }
-}
+    pop(){
+        if(!this.first) return undefined
+        let current=this.first
+        let newLast=this.first
+        while(current.next){
+            newLast=current
+            current=current.next
+        }
+           this.last=newLast;
+           this.last.next=null
+           this.length--
+           if(this.length===0){
+            this.first=null
+            this.last=null
+           }
+            return current
+        }
+
+        unshift(value){
+               
+            let newNode=new Node(value)
+            if(!this.first){
+                this.first=newNode
+                this.last=this.first
+               
+                
+            }
+            else{
+                newNode.next=this.first
+                this.first=newNode
+
+            }
+           
+            this.length++
+            return this
+    }
+    }
 
 
 
