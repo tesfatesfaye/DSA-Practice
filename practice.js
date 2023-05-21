ee23d6b818c0f9234e319c1213f5c8b956a74e3b
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-21 02:05:19 -0400

    implemnted stack with an array

---
+++ practice.js
@@ -0,0 +1,11 @@
+//Array implementation example
+const arrayStack=[]
+arrayStack.push('valueOne')
+arrayStack.push('valueTwo')
+arrayStack.push('valueThree')
+arrayStack.push('valueFour')
+arrayStack.push('valueFive')
+arrayStack.pop()
+arrayStack.pop()
+arrayStack.push("hello")
+arrayStack.pop() // removes and returns hello
