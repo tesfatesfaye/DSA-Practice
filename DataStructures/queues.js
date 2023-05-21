0dfaef716b79057e439549ee61ee63146207880e
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-21 03:42:46 -0400

    moved queues implemnation through arrays in it's own file

---
+++ DataStructures/queues.js
@@ -0,0 +1,28 @@
+// queues with arrays
+//queues espouse the principal first in first out
+const queuesArray=[]
+// we could either do unshift + pop or we could either do shift + push
+queuesArray.push(0)
+queuesArray.push(1)
+queuesArray.push(2)
+queuesArray.push(3)
+//after adding via push we remove using shift
+queuesArray.shift()
+queuesArray.shift()
+queuesArray.shift()
+queuesArray.shift()
+
+const queuesArrayTwo=[]
+// we do the same thing the other way around
+queuesArray.unshift(0)
+queuesArray.unshift(1)
+queuesArray.unshift(2)
+queuesArray.unshift(3)
+// removing the inserted values
+queuesArray.pop()
+queuesArray.pop()
+queuesArray.pop()
+queuesArray.pop()
+
+
+
