64af588159b22824cda9780a5495a884b88cf753
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-21 02:06:19 -0400

    implemented second array stack albeit less performative

--- practice.js
+++ practice.js
@@ -9,3 +9,10 @@ arrayStack.pop()
 arrayStack.pop()
 arrayStack.push("hello")
 arrayStack.pop() // removes and returns hello
+// a second implementation
+const arrayStackTwo=[]
+arrayStackTwo.unshift('valueOne')
+arrayStackTwo.unshift('valueTwo')
+arrayStackTwo.unshift('valueThere')
+arrayStackTwo.unshift('valueFour')
+arrayStackTwo.shift()// removes "value four" and returns it
