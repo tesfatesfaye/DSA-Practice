80000b88a6b79f27f105d8ff5660f56a9ad31c10
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-21 03:36:37 -0400

    implmented one version of queues using arrays

---
+++ DataStructures/stacks.js
@@ -0,0 +1,65 @@
+//Array implementation example
+const arrayStack=[]
+arrayStack.push('valueOne')
+arrayStack.push('valueTwo')
+arrayStack.push('valueThree')
+arrayStack.push('valueFour')
+arrayStack.push('valueFive')
+arrayStack.pop()
+arrayStack.pop()
+arrayStack.push("hello")
+arrayStack.pop() // removes and returns hello
+// a second implementation
+const arrayStackTwo=[]
+arrayStackTwo.unshift('valueOne')
+arrayStackTwo.unshift('valueTwo')
+arrayStackTwo.unshift('valueThere')
+arrayStackTwo.unshift('valueFour')
+arrayStackTwo.shift()// removes "value four" and returns it
+
+// linked list stack
+class Node {
+    constructor(val){
+        this.val=val
+        this.next=null
+    }
+}
+class stack{ //since push and pop have to be O(1) time we will use shift and unshift albeit with their names changed appropriately 
+    constructor(){
+        this.first=null;
+        this.last=null;
+        this.size=0;
+    }
+
+        pop(){
+            if(this.size===0) return undefined
+            let oldNode=this.first
+            this.first=oldNode.next
+           this.size --
+                if(this.size===0){
+                    this.last=null
+                }
+            return oldNode
+
+        }
+        push(val){
+            let newNode=new Node(val)
+            if(this.size===0){
+               this.first=newNode
+                this.last=newNode
+                }
+                else{
+                    newNode.next=this.first
+                    this.first=newNode
+                }
+              
+                return  ++ this.size
+
+        }
+        
+}
+const stackExample=new stack()
+stackExample.push(12)
+stackExample.push(11)
+stackExample.push(2)
+stackExample.pop()
--- practice.js
+++ practice.js
@@ -1,60 +1,19 @@
-//Array implementation example
-const arrayStack=[]
-arrayStack.push('valueOne')
-arrayStack.push('valueTwo')
-arrayStack.push('valueThree')
-arrayStack.push('valueFour')
-arrayStack.push('valueFive')
-arrayStack.pop()
-arrayStack.pop()
-arrayStack.push("hello")
-arrayStack.pop() // removes and returns hello
-// a second implementation
-const arrayStackTwo=[]
-arrayStackTwo.unshift('valueOne')
-arrayStackTwo.unshift('valueTwo')
-arrayStackTwo.unshift('valueThere')
-arrayStackTwo.unshift('valueFour')
-arrayStackTwo.shift()// removes "value four" and returns it
+// queues with arrays
+//queues espouse the principal first in first out
+const queuesArray=[]
+// we could either do unshift + pop or we could either do shift + push
+queuesArray.push(0)
+queuesArray.push(1)
+queuesArray.push(2)
+queuesArray.push(3)
+//after adding via push we remove using shift
+queuesArray.shift()
+queuesArray.shift()
+queuesArray.shift()
+queuesArray.shift()
 
-// linked list stack
-class Node {
-    constructor(val){
-        this.val=val
-        this.next=null
-    }
-}
-class stack{ //since push and pop have to be O(1) time we will use shift and unshift albeit with their names changed appropriately 
-    constructor(){
-        this.first=null;
-        this.last=null;
-        this.size=0;
-    }
 
-        pop(){
-            if(this.size===0) return undefined
-            let oldNode=this.first
-            this.first=oldNode.next
-           this.size --
-                if(this.size===0){
-                    this.last=null
-                }
-            return oldNode
 
-        }
-        push(val){
-            let newNode=new Node(val)
-            if(this.size===0){
-               this.first=newNode
-                this.last=newNode
-                }
-                else{
-                    newNode.next=this.first
-                    this.first=newNode
-                }
-                this.size ++
-                return this
 
-        }
-        
-}
+
+
