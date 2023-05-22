6459899e61c081ababafbc75353eebf7ed319e8f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-22 14:25:59 -0400

    created BST node

--- practice.js
+++ practice.js
@@ -6,3 +6,20 @@ class Node {
     }
 }
 
+class BinarySearchTree{
+    constructor(){
+        this.root=null
+    }
+    
+
+
+
+}
+
+let tree=new BinarySearchTree();
+tree.root=new Node(10);
+tree.root.right=new Node(15);
+tree.root.left=new Node(7)
+tree.root.left.right=new Node(9)
+
+console.log(tree)
