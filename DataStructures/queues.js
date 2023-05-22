c82a0a4d167bd9e3eec13bd88d2c650ba253ca55
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-22 13:26:55 -0400

    created trees js file

--- DataStructures/queues.js
+++ DataStructures/queues.js
@@ -26,3 +26,59 @@ queuesArray.pop()
 
 
 
+
+//proper queue
+class Node {
+    constructor(val){
+        this.val=val
+        this.next=null
+    }
+}
+class Queue{ //since push and pop have to be O(1) time we will use shift and unshift albeit with their names changed appropriately 
+    constructor(){
+        this.first=null;
+        this.last=null;
+        this.size=0;
+    }
+ 
+    dequeue(){ // unshift
+        if(!this.first)return undefined
+        let current=this.first
+        this.first=current.next
+        this.size--
+        if(this.size===0){
+             this.last=null
+        }
+        return current
+    }
+    //enqueue
+    enqueue(val){
+        let newNode=new Node(val)
+        if(this.first===null){
+            this.first=newNode
+            this.last=newNode
+           
+        }
+        else {
+            this.last.next=newNode
+            this.last=newNode
+          
+        }
+       
+            return ++this.size;
+
+    }
+    }
+
+const queueClass=new Queue()
+queueClass.enqueue(1)
+queueClass.enqueue(2)
+queueClass.enqueue(3)
+console.log(queueClass)
+queueClass.dequeue()
+console.log(queueClass)
+
+
+
+
+
