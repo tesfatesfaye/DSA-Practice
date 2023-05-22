b19981f2df7eaf0278ebc3e67fd60e326ab0480f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-22 15:40:48 -0400

    cleaned up insert function

--- practice.js
+++ practice.js
@@ -1,53 +1,55 @@
 class Node {
     constructor(val) {
-        this.val=val
-        this.left=null
-        this.right=null
+        this.val = val
+        this.left = null
+        this.right = null
     }
 }
 
-class BinarySearchTree{
-    constructor(){
-        this.root=null
+class BinarySearchTree {
+    constructor() {
+        this.root = null
     }
-    
-    insert(value){
-        let newNode=new Node(value)
-        if(this.root===null){
-            this.root=newNode
-            return this
+
+    insert(value) {
+        let newNode = new Node(value)
+        if (this.root === null) {
+            this.root = newNode
         }
-            let node=this.root
-            let inserted=false
-            while(!inserted){
-                if(node.val>value){
-                    if(node.left!==null){
-                        node=node.left
+        else {
+
+            let node = this.root
+           
+            while (true) {
+                if (node.val > value) {
+                    if (node.left !== null) {
+                        node = node.left
                     }
-                    else{
-                        node.left=newNode
-                        inserted=true
+                    else {
+                        node.left = newNode
+                        break;
                     }
                 }
 
                 else {
-                    if(node.right!==null){
-                        node=node.right
+                    if (node.right !== null) {
+                        node = node.right
                     }
                     else {
-                        node.right=newNode
-                        inserted=true
+                        node.right = newNode
+                        break;
                     }
                 }
             }
-            return this
+        }
+        return this
 
     }
 
 
 }
 
-let tree=new BinarySearchTree();
+let tree = new BinarySearchTree();
 tree.insert(10)
 tree.insert(5)
 tree.insert(13)
