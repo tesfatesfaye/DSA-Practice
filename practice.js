0329f9934833b19b60b20bbe20218f6934ee54ce
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-26 15:59:29 -0400

    implemented a binary max heap with an insert function

---
+++ practice.js
@@ -0,0 +1,28 @@
+class MaxBinaryHeap{
+constructor(){
+    this.value=[]
+}
+    insert(val){
+        this.value.push(val)
+        if(this.value.length===1) return this.value
+        let index=this.value.length-1
+        const parent=(x)=>{
+          return (Math.floor((x-1)/2))
+        }
+            while(index){
+                let parentIn=parent(index)       
+            if(val>this.value[parentIn]){
+                [this.value[parentIn],this.value[index]]=[this.value[index],this.value[parentIn]]
+                index=parentIn
+               
+            }
+            }
+            return this.value
+    }
+
+}
+
+const max= new MaxBinaryHeap()
+console.log(max.insert(5))
+console.log(max.insert(6))
+console.log(max.insert(7))
