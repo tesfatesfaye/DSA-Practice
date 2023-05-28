2e4cfc8c94d232ec6addf2501e7da879bf24bef7
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-28 13:37:41 -0400

    implemented basic hash function

---
+++ practice.js
@@ -0,0 +1,12 @@
+//basic hash function
+let array=[]
+const hash=(key,arrayLen)=>{
+    let total=0;
+    for(let char of key){
+        let value=char.charCodeAt(0)-96
+        total=total+value
+    }
+    return (total)
+
+}
+console.log(hash("tesfa",array.length))
