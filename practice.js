70f05cf835e079ce349345f849348eb9cfc0189f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-28 13:39:02 -0400

    fixed issue with the total variable

--- practice.js
+++ practice.js
@@ -4,7 +4,7 @@ const hash=(key,arrayLen)=>{
     let total=0;
     for(let char of key){
         let value=char.charCodeAt(0)-96
-        total=total+value
+        total=(total+value) %arrayLen
     }
     return (total)
 
