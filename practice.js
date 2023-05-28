6486d64326fb9d3ddd659289ad5a14528dbb0d87
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-28 15:16:23 -0400

    implemented a set function for the hashmap class

--- practice.js
+++ practice.js
@@ -13,19 +13,37 @@
 
 // }
 
-class HashTable{
-    constructor(size=53){
-        this.keyMap=new Array(size);
+class HashTable {
+    constructor(size = 53) {
+        this.keyMap = new Array(size);
     }
-    _hash(key){
-        let total=0;
-        let WEIRD_PRIME=31;
-        for (let i=0; i< Math.min(key.length,100);i++){
-            let char=key[i]
-            let value=char.charCodeAt(0)-96
-            total=(total*WEIRD_PRIME +value) % this.keyMap.length;
+    _hash(key) {
+        let total = 0;
+        let WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
         }
         return total;
     }
 
-}
+    set(key) {
+        let hashedValue = this._hash(key)
+        if (!this.keyMap[hashedValue]) {
+            this.keyMap[hashedValue] = key
+        }
+        else {
+            if (Array.isArray[keyMap[hashedValue]]) {
+                (this.keyMap[hashedValue]).push(key)
+            }
+            else {
+                this.keyMap[hashedValue] = [this.keyMap[hashedValue], key]
+            }
+        }
+        return this.keyMap
+    }
+
+
+    }
+
