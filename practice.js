f766fbf53b6dba1ade400ab28679819d7f2ebe73
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-28 14:37:59 -0400

    converted has function to class

--- practice.js
+++ practice.js
@@ -1,14 +1,31 @@
 //basic hash function
-let array=10
-const hash=(key,arrayLen)=>{
-    let total=0
-    let prime=31
-    for(let i=0;i<Math.min(key.length,100);i++){
-        let char=key[i]
-        let value=char.charCodeAt(0)-96
-        total=(total*prime+value)% arrayLen;
+// let array=10
+// const hash=(key,arrayLen)=>{
+//     let total=0
+//     let prime=31
+//     for(let i=0;i<Math.min(key.length,100);i++){
+//         let char=key[i]
+//         let value=char.charCodeAt(0)-96
+//         total=(total*prime+value)% arrayLen;
 
+//     }
+//     return (total)
+
+// }
+
+class HashTable{
+    constructor(size=53){
+        this.keyMap=new Array(size);
     }
-    return (total)
+    _hash(key){
+        let total=0;
+        let WEIRD_PRIME=31;
+        for (let i=0; i< Math.min(key.length,100);i++){
+            let char=key[i]
+            let value=char.charCodeAt(0)-96
+            total=(total*WEIRD_PRIME +value) % this.keyMap.length;
+        }
+        return total;
+    }
 
 }
