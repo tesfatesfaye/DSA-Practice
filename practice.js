cc6e4bb41bf72fa5f50ebe41fc8e5b99ddbeee19
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-01 15:38:05 -0400

    implented removeEdge function

--- practice.js
+++ practice.js
@@ -1,29 +1,31 @@
-class Graph{// adjacency list undirected
-    constructor(){
-        this.adjacencyList={}
+class Graph {// adjacency list undirected
+    constructor() {
+        this.adjacencyList = {}
     }
 
-    addVertex(vertex){
-            if(!this.adjacencyList[vertex]){
-            this.adjacencyList[vertex]=[]
-            }
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = []
+        }
     }
-    addEdge(vertexOne,vertexTwo){
-        if(this.adjacencyList[vertexOne]&&this.adjacencyList[vertexTwo]){
-            
-            if((this.adjacencyList[vertexOne].every(x=>x!==vertexTwo))&&(this.adjacencyList[vertexTwo].every(x=>x!==vertexOne))){
-             
+    addEdge(vertexOne, vertexTwo) {
+        if (this.adjacencyList[vertexOne] && this.adjacencyList[vertexTwo]) {
+            if ((this.adjacencyList[vertexOne].every(x => x !== vertexTwo)) && (this.adjacencyList[vertexTwo].every(x => x !== vertexOne))) {
                 this.adjacencyList[vertexOne].push(vertexTwo)
                 this.adjacencyList[vertexTwo].push(vertexOne)
             }
 
         }
     }
+    removeEdge(vertexOne, vertexTwo) {
+        this.adjacencyList[vertexOne]=this.adjacencyList.filter(x=>x!==vertexTwo)
+        this.adjacencyList[vertexTwo]=this.adjacencyList.filter(x=>x!==vertexOne)
+    }
 
 }
 
-let g=new Graph()
+let g = new Graph()
 g.addVertex("addis")
 g.addVertex("paris")
-g.addEdge("addis","paris")
+g.addEdge("addis", "paris")
 console.log(g)
