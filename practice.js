4efcf8bd6aa97e8cbcb8ef319751d5aad17e6f8d
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-01 19:24:05 -0400

    implemented depth first function for graph

--- practice.js
+++ practice.js
@@ -27,19 +27,42 @@ class Graph {// adjacency list undirected
         delete this.adjacencyList[vertex]
         }
     }
+    dfsR(vertex,list=[],marker={}){
+       if(this.adjacencyList[vertex]){
+            if(!marker[vertex]){
+                marker[vertex]=true
+                list.push(vertex)
+            }
+            for(let i of this.adjacencyList[vertex]){
+                if(!marker[i]){
+                    list.push(i)
+                    marker[i]=true
+                    this.dfsR(i,list,marker)
+                }
+            }
+       }
+       else{
+        return null
+       }
+        return list
+    }
 
 }
 
 let g = new Graph()
-g.addVertex("addis")
-g.addVertex("paris")
-g.addVertex("london")
-g.addVertex("tokyo")
-g.addEdge("addis", "paris")
-g.addEdge("addis","tokyo")
-g.addEdge("addis","london")
-g.addEdge("paris","london")
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
 
 console.log(g.adjacencyList)
-g.removeVertex("addis")
-console.log(g.adjacencyList)
+console.log(g.dfsR("A"))
