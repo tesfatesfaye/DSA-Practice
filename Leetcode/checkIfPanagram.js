c86231aa38d511489811f849f0187a82e49adfcb
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-03 17:25:27 -0400

    changed name of attribute for the checkIfPangram function to fit the context

--- Leetcode/checkIfPanagram.js
+++ Leetcode/checkIfPanagram.js
@@ -1,8 +1,8 @@
-const checkIfPangram=(unLowered)=>{
-    if(unLowered.length<26) return false // will return false if the length is less than the number of letters in the alphabet
+const checkIfPangram=(sentence)=>{
+    if(sentence.length<26) return false // will return false if the length is less than the number of letters in the alphabet
       let alphabetObject={}
-    for(let i=0;i<unLowered.length;i++){ 
-        alphabetObject[unLowered[i]]=1
+    for(let i=0;i<sentence.length;i++){ 
+        alphabetObject[sentence[i]]=1
     }
     if(Object.keys(alphabetObject).length===26){ 
         // after every letter has been accounted for 
