fc9d861d86a4722a5afb671ab4315914fc291f12
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-08 15:59:29 -0400

    implemented add vertext function for the weighted graph class

--- practice.js
+++ practice.js
@@ -2,4 +2,7 @@ class WeightedGraph{
     constructor(){
         this.adjacencyList={}
     }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex]=[]
+    }
 }
