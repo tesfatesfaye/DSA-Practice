3dc93aaf0592708b117d2c70b315ac93d7daa8ba
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-08 16:02:59 -0400

    added an addEdge function

--- practice.js
+++ practice.js
@@ -5,4 +5,14 @@ class WeightedGraph{
     addVertex(vertex){
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex]=[]
     }
+    addEdge(vertexOne, vertexTwo,weight) {
+        if (this.adjacencyList[vertexOne] && this.adjacencyList[vertexTwo]) {
+            if ((this.adjacencyList[vertexOne].every(x => x !== vertexTwo)) && 
+            (this.adjacencyList[vertexTwo].every(x => x !== vertexOne))) {
+                this.adjacencyList[vertexOne].push({node:vertexTwo,weight})
+                this.adjacencyList[vertexTwo].push({node:vertexOne,weight})
+            }
+        }
+    }
 }
+
