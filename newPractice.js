8c3982bd2af00bf27277f9db81638fe80c36484c
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-11 11:04:51 -0400

    created a simple priority queue class

--- newPractice.js
+++ newPractice.js
@@ -1,1 +1,15 @@
-// create a function that can find the median value of an array 
+class PriorityQueue {
+    constructor(){
+      this.values = [];
+    }
+    enqueue(val, priority) {
+      this.values.push({val, priority});
+      this.sort();
+    };
+    dequeue() {
+      return this.values.shift();
+    };
+    sort() {
+      this.values.sort((a, b) => a.priority - b.priority);
+    };
+  }
