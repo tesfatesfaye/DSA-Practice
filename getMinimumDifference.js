71892477c21a2b40cc41a13b4ef008f792d24cd0
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-12 13:21:39 -0400

    cleaned up the function

--- getMinimumDifference.js
+++ getMinimumDifference.js
@@ -1,8 +1,7 @@
-var getMinimumDifference = function(root) {
+const getMinimumDifference = function(root) {
 
-    let node=root
     let difference=Number.POSITIVE_INFINITY
-    if(node.left===null&&node.right===null) return 0
+    if(root.left===null&&root.right===null) return 0
     const traverse=(current,previous=null)=>{
        
         if(current.left) traverse(current.left,current)
@@ -10,13 +9,10 @@ var getMinimumDifference = function(root) {
             difference=Math.min(current.val-previous.val)
         }
        
-        if(current.right)traverse (current.right)
+        if(current.right)traverse (current.right,current)
     }
     traverse(root)
     
-        for(let i=0;i<arr.length-1;i++){
-            difference=Math.min((arr[i+1]-arr[i]),difference)
-        }
-        console.log(arr)
+       
     return difference
 };
