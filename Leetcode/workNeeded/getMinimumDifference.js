36bfbcd15d6e1a1eed30b7fd21610f5561b1a4ab
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-14 14:11:37 -0400

    optimized getMinDifference function

--- Leetcode/workNeeded/getMinimumDifference.js
+++ Leetcode/workNeeded/getMinimumDifference.js
@@ -1,38 +1,42 @@
-const getMinimumDifference = (root) => {
-    let arr=[]
-    let node=root
-    let difference=Number.POSITIVE_INFINITY
-    if(node.left===null&&node.right===null) return 0
-    const traverse=(current)=>{
-        if(current.left) traverse(current.left)
-        arr.push(current.val)
-        if(current.right)traverse (current.right)
+
+// this needs work to be optimized
+// const getMinimumDifference = (root) => {
+//     let arr=[]
+//     let node=root
+//     let difference=Number.POSITIVE_INFINITY
+//     if(node.left===null&&node.right===null) return 0
+//     const traverse=(currentNode)=>{
+//         if(currentNode.left) traverse(currentNode.left)
+//         arr.push(currentNode.val)
+//         if(currentNode.right)traverse (currentNode.right)
+//     }
+//     traverse(root)
+    
+//         for(let i=0;i<arr.length-1;i++){
+//             difference=Math.min((arr[i+1]-arr[i]),difference)
+//         }
+//         console.log(arr)
+//     return difference
+// };
+
+const getMinimumDifference =(root)=> {
+    // you mostly get it but you need some work on it
+
+    
+    const traverse=(currentNode)=>{
+       if(!currentNode) return null
+       traverse(currentNode.left)
+        if(previousNode){
+            minDifference=Math.min(currentNode.val-previousNode.val,minDifference)
+        }
+            previousNode=currentNode
+        traverse (currentNode.right)
     }
+    let previousNode=null
+    let minDifference=Infinity
     traverse(root)
     
-        for(let i=0;i<arr.length-1;i++){
-            difference=Math.min((arr[i+1]-arr[i]),difference)
-        }
-        console.log(arr)
-    return difference
-};
-
-const getMinimumDifferenceTwo =(root)=> {
-
-    let difference=Number.POSITIVE_INFINITY
-    if(root.left===null&&root.right===null) return 0
-    const traverse=(current,previous=null)=>{
        
-        if(current.left) traverse(current.left,current)
-        if(previous){
-            difference=Math.min(Math.abs(current.val-previous.val),difference)
-        }
-       
-        if(current.right)traverse (current.right,current)
-    }
-    traverse(root)
-    
-       
-    return difference
+    return minDifference
 };
 
