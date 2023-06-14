8508d0070bc0978b3950ce97cae1ba2c78b6dbdf
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-14 13:39:19 -0400

    renamed function

--- Leetcode/workNeeded/getMinimumDifference.js
+++ Leetcode/workNeeded/getMinimumDifference.js
@@ -1,4 +1,23 @@
-const getMinimumDifference =(root)=> {
+var getMinimumDifference = function(root) {
+    let arr=[]
+    let node=root
+    let difference=Number.POSITIVE_INFINITY
+    if(node.left===null&&node.right===null) return 0
+    const traverse=(current)=>{
+        if(current.left) traverse(current.left)
+        arr.push(current.val)
+        if(current.right)traverse (current.right)
+    }
+    traverse(root)
+    
+        for(let i=0;i<arr.length-1;i++){
+            difference=Math.min((arr[i+1]-arr[i]),difference)
+        }
+        console.log(arr)
+    return difference
+};
+
+const getMinimumDifferenceTwo =(root)=> {
 
     let difference=Number.POSITIVE_INFINITY
     if(root.left===null&&root.right===null) return 0
@@ -16,3 +35,4 @@ const getMinimumDifference =(root)=> {
        
     return difference
 };
+
