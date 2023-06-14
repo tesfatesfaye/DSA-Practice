d02fa2bca70666ab11cbce17cbb04eb9dcee5ff3
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-14 15:26:03 -0400

    moved priority queue code to practice js file

--- practice.js
+++ practice.js
@@ -1,39 +1,59 @@
-class WeightedGraph{
+class PriorityQueue {
     constructor(){
-        this.adjacencyList={}
+      this.values = [];
     }
-    addVertex(vertex){
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex]=[]
+    enqueue(val, priority) {
+      this.values.push({val, priority});
+      this.sort();
+    };
+    dequeue() {
+      return this.values.shift();
+    };
+    sort() {
+      this.values.sort((a, b) => a.priority - b.priority);
+    };
+  }
+
+  
+class WeightedGraph {
+    constructor() {
+        this.adjacencyList = {}
     }
-    addEdge(vertexOne, vertexTwo,weight) {
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+    }
+    addEdge(vertexOne, vertexTwo, weight) {
         if (this.adjacencyList[vertexOne] && this.adjacencyList[vertexTwo]) {
-            if ((this.adjacencyList[vertexOne].every(x => x !== vertexTwo)) && 
-            (this.adjacencyList[vertexTwo].every(x => x !== vertexOne))) {
-                this.adjacencyList[vertexOne].push({node:vertexTwo,weight})
-                this.adjacencyList[vertexTwo].push({node:vertexOne,weight})
+            if ((this.adjacencyList[vertexOne].every(x => x !== vertexTwo)) &&
+                (this.adjacencyList[vertexTwo].every(x => x !== vertexOne))) {
+                this.adjacencyList[vertexOne].push({ node: vertexTwo, weight })
+                this.adjacencyList[vertexTwo].push({ node: vertexOne, weight })
             }
         }
     }
-    
-    
+    dijkstra(start, finish) {
+
+    }
+
 
 }
 
-let graph= new WeightedGraph()
+let graph = new WeightedGraph()
 graph.addVertex("A")
 graph.addVertex("B")
 graph.addVertex("C")
 graph.addVertex("D")
 graph.addVertex("E")
 graph.addVertex("F")
-graph.addEdge("A","B",4)
-graph.addEdge("A","C",2)
-graph.addEdge("B","E",3)
-graph.addEdge("C","D",2)
-graph.addEdge("C","F",4)
-graph.addEdge("D","E",3)
-graph.addEdge("D","F",1)
-graph.addEdge("E","F",1)
+
+graph.addEdge("A", "B", 4)
+graph.addEdge("A", "C", 2)
+graph.addEdge("B", "E", 3)
+graph.addEdge("C", "D", 2)
+graph.addEdge("C", "F", 4)
+graph.addEdge("D", "E", 3)
+graph.addEdge("D", "F", 1)
+graph.addEdge("E", "F", 1)
 console.log(graph.adjacencyList)
 
 
