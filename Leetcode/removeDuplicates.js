fc6919d03a4a4070563db987453f4a676313b15c
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-14 18:16:04 -0400

    moved removeDuplicates to appropriate folder

--- Leetcode/removeDuplicates.js
+++ Leetcode/removeDuplicates.js
@@ -1,47 +1,90 @@
-var removeDuplicates = function(nums) {
-    let obj={}
-    let neg={}
-    let removedDuplicatesArray=[]
 
-for(let i=0;i<nums.length;i++){
-    if(nums[i]<0){
-        if(!neg[nums[i]]){
-            neg[nums[i]]=1
+
+
+const removeDuplicates = (nums) =>{
+    if(nums.length===0) return 0
+    let start=0
+    for(let i=1;i<nums.length;i++){
+        if(nums[i]!==nums[start]){
+            nums[start+1]=nums[i]
+            start++
+
         }
     }
-    else{
-    if(!obj[nums[i]]){
-        obj[nums[i]]=1
-        
+    
 
-    }
-    }
-       
-}
+    return start+1
 
- if(Object.keys(neg).length>0){
-            for(let i in neg){
-                removedDuplicatesArray.push(parseInt(i))
-                }
-            
-    }
-
-    if(Object.keys(obj).length>0){
-        for(let i in obj){
-                removedDuplicatesArray.push(parseInt(i))
-                }
-            
-    }
-
-    nums.splice(0,nums.length)
-    for(let i of removedDuplicatesArray){
-        nums.push(i)
-    }
-
-// nums=removedDuplicatesArray
-return nums.length
 
 };
 
+console.log(removeDuplicates([1,2,2,3,3,4,5,12]))
 
-console.log(removeDuplicates([-2,-2,-1,0,1,1,1,2,2,3,3,4]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var removeDuplicates = function(nums) {
+//     let obj={}
+//     let neg={}
+//     let removedDuplicatesArray=[]
+
+// for(let i=0;i<nums.length;i++){
+//     if(nums[i]<0){
+//         if(!neg[nums[i]]){
+//             neg[nums[i]]=1
+//         }
+//     }
+//     else{
+//     if(!obj[nums[i]]){
+//         obj[nums[i]]=1
+        
+
+//     }
+//     }
+       
+// }
+
+//  if(Object.keys(neg).length>0){
+//             for(let i in neg){
+//                 removedDuplicatesArray.push(parseInt(i))
+//                 }
+            
+//     }
+
+//     if(Object.keys(obj).length>0){
+//         for(let i in obj){
+//                 removedDuplicatesArray.push(parseInt(i))
+//                 }
+            
+//     }
+
+//     nums.splice(0,nums.length)
+//     for(let i of removedDuplicatesArray){
+//         nums.push(i)
+//     }
+
+// // nums=removedDuplicatesArray
+// return nums.length
+
+// };
+
+
+// console.log(removeDuplicates([-2,-2,-1,0,1,1,1,2,2,3,3,4]))
