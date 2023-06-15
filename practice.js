0790ac1339497b612c89a9f4d7aed749095919c2
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-15 17:12:26 -0400

    made changes to practice js file

--- practice.js
+++ practice.js
@@ -1,16 +1,8 @@
-const fib = (value) => {
-
+const fib = (num) => {
         const memo = { "0": 0, "1": 1 }
-        const helper = (num) => {
-            if (memo[num] !== undefined) return memo[num]
-                     return  memo[num] ?? (memo[num]=helper(num - 1) + helper(num - 2))
-        }
-
-        return (value > 1 ? helper(value): 0)
+        const helper = (num) => memo[num] ?? (memo[num]=helper(num - 1) + helper(num - 2))
+        return (num > 1 ? helper(num): 0)
+    }
     
-
-
-}
-
-console.log(fib(9))
+    console.log(fib(9))
 
