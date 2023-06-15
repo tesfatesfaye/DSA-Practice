b2fee8383310ac6ae62666220c1ab0c64ed403c1
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-15 17:40:16 -0400

    added fib function with no helper

--- practice.js
+++ practice.js
@@ -3,6 +3,10 @@ const fib = (num) => {
         const helper = (num) => memo[num] ?? (memo[num]=helper(num - 1) + helper(num - 2))
         return (num > 1 ? helper(num): 0)
     }
-    
-    console.log(fib(9))
+    console.log(fib(1))
 
+const fibNoHelper=(num,first=true,memo={"0":0,"1":1})=>{
+return (first&&num<=1) ? 0:memo[num] ?? (memo[num]=fibNoHelper(num-1,false)+fibNoHelper(num-2,false))
+   }
+
+console.log(fibNoHelper(3))
