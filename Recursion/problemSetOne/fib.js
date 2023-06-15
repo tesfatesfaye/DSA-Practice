1e9b5798b5bccfa24e8b959125b5981dece4d766
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-15 19:14:59 -0400

    cleaned up fib function

--- Recursion/problemSetOne/fib.js
+++ Recursion/problemSetOne/fib.js
@@ -1,5 +1,5 @@
     const fibTwo=(n)=>{
-    if(n===0|| n===1) return n
+  if(n<=2) return 1
     
     return fib(n-1)+fib(n-2)
     
--- practice.js
+++ practice.js
@@ -1,12 +1,16 @@
 const fib = (num) => {
-        const memo = { "0": 0, "1": 1 }
-        const helper = (num) => memo[num] ?? (memo[num]=helper(num - 1) + helper(num - 2))
-        return (num > 1 ? helper(num): 0)
+    if(num<=0) return 0
+    const memo = { "1": 1, "2": 1 }
+    const helper = (num) => {
+        if (num <= 2) return memo[num] 
+        return memo[num] ?? (memo[num] = helper(num - 1) + helper(num - 2))
     }
-    console.log(fib(1))
+    return helper(num)
+}
+console.log(fib(2))
 
-const fibNoHelper=(num,first=true,memo={"0":0,"1":1})=>{
-return (first&&num<=1) ? 0:memo[num] ?? (memo[num]=fibNoHelper(num-1,false)+fibNoHelper(num-2,false))
-   }
+const fibNoHelper = (num, memo = { "1": 1, "2": 1 }) => {
+    return ( num===0) ? num : memo[num] ?? (memo[num] = fibNoHelper(num - 1) + fibNoHelper(num - 2))
+}
 
-console.log(fibNoHelper(3))
+console.log(fibNoHelper(6)) 
