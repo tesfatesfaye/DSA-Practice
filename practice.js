1bc747350c2801e414588fcdffa0b07b81608b5c
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-16 01:13:44 -0400

    optimized maxProfit function

--- practice.js
+++ practice.js
@@ -1,14 +1,21 @@
-var maxProfit = function(prices) {
+const maxProfit = (prices)=> {
     let highPrice=0
     let end=prices.length-1
-    let counter=1
-    for(let i=end;i>0;i--){
-        highPrice=Math.max(highPrice,(prices[end]-prices[i-1]))
-        if(prices[end]<prices[i-1]){
-            end=i-1
+     for(let i=end;i>0;i--){
+        let left=prices[i-1]
+        if(prices[end]<=left){
+             end=i-1
+                    }
+        else{
+        highPrice=Math.max(highPrice,prices[end]-left)
         }
-
+       
+        
     }
+ 
+
       return highPrice
 
 };
+
+console.log(maxProfit(7,6,4,3,1))
