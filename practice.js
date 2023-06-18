22090ef1552ed36bab2c8bfb20ccfc95f3d874ea
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-18 18:26:37 -0400

    added a second variant of the two sum problem

--- practice.js
+++ practice.js
@@ -12,3 +12,32 @@ let difference=target-value
     }
 
 }
+
+
+const twoSumCrude = (nums, target) =>{
+    let hashMap={}
+    for(let i=0;i<nums.length;i++){
+        if(!hashMap[nums[i]]){
+        hashMap[nums[i]]=[i]
+        }
+        else{
+          
+            hashMap[nums[i]].push(i)
+                
+        }
+    }
+
+    for(let i in hashMap){
+        let item=target-i
+        if(item==i && hashMap[i].length>1){
+            console.log(hashMap[i][1])
+            return[hashMap[i][0],hashMap[i][1]]
+        }
+        if(hashMap[item]!==undefined){
+            return[hashMap[i][0],hashMap[item][0]]
+        }
+    }
+
+
+
+};
