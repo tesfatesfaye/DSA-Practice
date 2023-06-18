7b3117ab4ab1edcd5d9fd8923370f1593dd8636f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-18 18:27:28 -0400

    added a third implementation uses maps as opposed to objects

--- practice.js
+++ practice.js
@@ -41,3 +41,34 @@ const twoSumCrude = (nums, target) =>{
 
 
 };
+
+
+const twoSumMap = (nums, target) =>{
+    let map=new Map()
+    for(let i=0;i<nums.length;i++){
+        if(!map.has(nums[i])){
+        map.set(nums[i],[i])
+        }
+        else{
+          
+            map.get(nums[i]).push(i)
+                
+        }
+    }
+        console.log(map)
+    for(let i of map){
+        let key=i[0]
+     
+        let item=target-key
+                if(item===key&& map.get(key).length>1){
+          
+            return[map.get(key)[0],map.get(key)[1]]
+        }
+        if(map.has(item) && item!==key){
+            return[map.get(item)[0],map.get(key)[0]]
+        }
+    }
+
+
+
+};
