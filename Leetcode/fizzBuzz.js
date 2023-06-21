3c8bd27ebff5b89817f0b29c871f22268d3d880d
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-21 18:06:02 -0400

    created and solved fizzbuzz function

---
+++ Leetcode/fizzBuzz.js
@@ -0,0 +1,23 @@
+var fizzBuzz = function(n) {
+
+    let printValue=""
+    let arry=[]
+    for(let i=1;i<=n;i++){
+        if(i%3===0){
+            printValue="Fizz"
+        }
+        if(i%5===0){
+            printValue+="Buzz"
+        }
+        
+        if(!printValue.length){
+             arry.push(i.toString())
+        }
+        else{
+            arry.push(printValue)
+        }
+        printValue=""
+    }
+        return arry
+    
+    };
