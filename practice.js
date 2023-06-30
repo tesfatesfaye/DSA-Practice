334ad1a5bf3e464e9cc0d1e89bbd4b2922b3c013
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-06-30 15:21:14 -0400

    implemetned new quickSort function

---
+++ practice.js
@@ -0,0 +1,32 @@
+const quickSort=(array)=>{
+if(array.length===1){
+    return array;
+}
+
+const pivot=array[array.length-1];
+const leftArray=[]
+const rightArray=[]
+for(let i=0;i<array.length-1;i++){
+    if(array[i] <pivot){
+        leftArray.push(array[i])
+    }
+    else{
+        rightArray.push(array[i])
+    }
+}
+if(leftArray.length>0 && rightArray.length>0){
+    return[...quickSort(leftArray),pivot,...quickSort(rightArray)]    
+
+}
+else if(leftArray.length>0){
+    return[...quickSort(leftArray),pivot]
+}
+else {
+    return[pivot,...quickSort(rightArray)]
+
+}
+
+}
+
+
+console.log(quickSort([5,12,8,9,6,2,11,5,3,4]))
