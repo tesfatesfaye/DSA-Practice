790fdb912fb40b341c00ef45606a40c4e9e7d437
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-07-02 20:42:40 -0400

    cleaned up the function

--- practice.js
+++ practice.js
@@ -1,28 +1,3 @@
-
-const pivot=(arr,left,right)=>{
-    const swap=(arr,one,two)=>{
-        [arr[one],arr[two]]=[arr[two],arr[one]]
-    }
-    let middle=Math.floor((left+right)/2)
-    swap(arr,left,right)
-    //swaps values 
-    let p=right
-    let j=left
-    let i=left-1
-    while(j<p){
-        if(arr[j]<arr[p]){
-            i++;
-            swap(arr,i,j)
-            }
-       
-            j++;
-        
-    }
-    i++;
-    swap(arr,i,p)
-    return i
-}
-
 const quickSort=(arr,left=0,right=arr.length-1)=>{
     if(left<right){
         let pivotIndex=pivot(arr,left,right)
@@ -31,6 +6,32 @@ const quickSort=(arr,left=0,right=arr.length-1)=>{
     }
     return arr
 }
+const pivot=(arr,left,right)=>{
+    const swap=(arr,one,two)=>{
+        [arr[one],arr[two]]=[arr[two],arr[one]]
+    }
+    let middle=Math.floor((left+right)/2)
+    swap(arr,middle,right)// swaps the middle and the right for randomization
+   
+    let p=right // takes the pivot index
+    let j=left // takes the first index value
+    let i=left-1 // the first index that is swapped with 
+    while(j<p){
+        if(arr[j]<arr[p]){ // if the a left most value is lower than the final pivot index, it has its index swapped 
+            i++;
+            swap(arr,i,j)
+            }
+       
+            j++;
+        
+    }
+    i++;
+    // all the lower values are set prior to the i index, so we swap whatever is on tha index with
+    swap(arr,i,p)
+    return i
+}
+
+
 
 console.log(quickSort([5,2,3,8,10,11,4,3,19,2,1,4]))
 
@@ -48,7 +49,7 @@ console.log(quickSort([5,2,3,8,10,11,4,3,19,2,1,4]))
 
 
 
-const quickSortMultiArray=(array)=>{
+const quickSortMultiArray=(array)=>{ 
 if(array.length<=1){
     return array;
 }
