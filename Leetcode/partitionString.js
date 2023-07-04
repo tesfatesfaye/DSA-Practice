8de756092fdc72eb790b2b314196aec5325666cd
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-07-03 21:27:29 -0400

    moved partition string to it's own directory

---
+++ Leetcode/partitionString.js
@@ -0,0 +1,19 @@
+const partitionString = (s) =>{
+    if(!s.length) return 0
+    let counter=1
+    let set=new Set()
+    for(let i=0;i<s.length;i++){
+        if(!set.has(s[i])){
+           set.add(s[i])
+        }
+        else{
+            set.clear()
+            set.add(s[i])
+            counter++
+                       
+        }
+    }
+    return counter
+};
+
+console.log(partitionString('abacaba'))
