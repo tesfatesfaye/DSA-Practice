9dc67d6cece1dbb8e3b445905f54231a815cb78f
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-15 15:12:25 -0400

    removed comments from valid phone number

--- Leetcode/validPhoneNumber.js
+++ Leetcode/validPhoneNumber.js
@@ -1,7 +1,7 @@
-// const valid=(num)=>{
-// let split = num.split("");
-// let obj = { "-": 1, "(": 1, ")": 1 };
-// split=split.filter(x=>!obj[x])
-// split = split.join("");
-// return ((split.length === 10 && !!parseInt(split)) ?? false);
-// }
+const valid=(num)=>{
+let split = num.split("");
+let obj = { "-": 1, "(": 1, ")": 1 };
+split=split.filter(x=>!obj[x])
+split = split.join("");
+return ((split.length === 10 && !!parseInt(split)) ?? false);
+}
