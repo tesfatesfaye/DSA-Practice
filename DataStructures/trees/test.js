2b982c3b2ce581387f5847b3542bb310e6e053f9
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-15 15:10:38 -0400

    moved missing number code to appropriate file

--- DataStructures/trees/test.js
+++ DataStructures/trees/test.js
@@ -19,12 +19,6 @@
 // }
 // console.log(validTwo("t-e,"))
 
-// var missingNumber = function (nums) {
-//   let sum = nums.reduce((x, y) => x + y);
-//   let n = nums.length+1;
-//   n = (n * (n + 1)) / 2;
-//   return n - sum ?? nums.length+1;
-// };
 
 // console.log(missingNumber([3,1,2]))
 
---
+++ Leetcode/missingNumber.js
@@ -0,0 +1,6 @@
+const missingNumber =  (nums)=> {
+  let sum = nums.reduce((x, y) => x + y);
+  let n = nums.length+1;
+  n = (n * (n + 1)) / 2;
+  return n - sum ?? nums.length+1;
+};
