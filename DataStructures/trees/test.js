65ab8a022bef4c51b9e95ad402c4798a09022906
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-15 15:11:02 -0400

    removed valid phone number from test js file

--- DataStructures/trees/test.js
+++ DataStructures/trees/test.js
@@ -1,13 +1,6 @@
 
 
 // let number="(123)-987-4567"
-// const valid=(num)=>{
-// let split = num.split("");
-// let obj = { "-": 1, "(": 1, ")": 1 };
-// split=split.filter(x=>!obj[x])
-// split = split.join("");
-// return ((split.length === 10 && !!parseInt(split)) ?? false);
-// }
 
 // console.log(valid(number))
 // console.log(valid("987-123-4567"));
