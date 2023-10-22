3c048b279a94804f811d03ca08eb004af94763bf
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-22 17:45:13 -0400

    corrected issue with maxiumu score

---
+++ Leetcode/maximumScore.js
@@ -0,0 +1,22 @@
+const maximumScore = (nums, k) => {
+  let l, r;
+  l = r = k;
+  let currMin = nums[k];
+  let currMax = nums[k];
+
+  while (l > 0 || r < nums.length - 1) {
+    let left = nums[l - 1] ?? 0;
+    let right = nums[r + 1] ?? 0;
+    if (left > right) {
+      l--;
+      currMin = Math.min(currMin, left);
+    } else {
+      r++;
+      currMin = Math.min(currMin, right);
+    }
+    currMax = Math.max(currMax, currMin * (r - l + 1));
+  }
+  return currMax;
+};
+
+console.log(maximumScore([5, 5, 4, 5, 4, 1, 1, 1], 0));
