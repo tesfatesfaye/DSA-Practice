7e95ffce1aac4fadfeb1a79de17ef5ac2b512c1a
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-22 21:15:50 -0400

    made several changes

---
+++ Leetcode/romanToInt.js
@@ -0,0 +1,22 @@
+var romanToInt = function (s) {
+  let ob = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (ob[s[i]] < ob[s[i + 1]]) {
+      total -= ob[s[i]];
+    } else {
+      total += ob[s[i]];
+    }
+  }
+  return total;
+};
+
+console.log(romanToInt("MCMXX"));
