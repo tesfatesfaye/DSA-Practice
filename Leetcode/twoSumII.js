945faed4455c6059aa3a2e23c0a7ddf39920bee5
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-25 14:45:43 -0400

    completed two sum ii and optimized two sum

---
+++ Leetcode/twoSumII.js
@@ -0,0 +1,17 @@
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      return [left + 1, right + 1];
+    }
+
+    if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+};
