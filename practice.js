7555a75ae9f0ce3991067db5aa1c8bbcdd7c76e3
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-25 17:32:09 -0400

    completed numtrees

--- practice.js
+++ practice.js
@@ -1,1 +1,18 @@
- 
+const numTrees=(n)=>{
+  let nFactorial=1
+  let twoNFactorial = 1;
+  for(let i=1;i<=n*2;i++){
+    twoNFactorial=twoNFactorial*i
+    if(i===n){
+      nFactorial=twoNFactorial
+    }
+
+  }
+
+  let value=twoNFactorial/((nFactorial*(n+1))*nFactorial)
+
+  return value
+
+}
+
+console.log(numTrees(3))
