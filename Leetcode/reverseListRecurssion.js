773a589dc3f54d70a61237e00414ef9a56565255
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-25 20:52:01 -0400

    made changes to reverse list recurssive function

--- Leetcode/reverseListRecurssion.js
+++ Leetcode/reverseListRecurssion.js
@@ -1,7 +1,6 @@
-var reverseList = function (head, next = null, prev = null) {
-  if (!head) return null;
-  next = head.next;
+const reverseList = (head, prev = null) => {
+  if (!head) return prev;
+  const next = head.next;
   head.next = prev;
-  let val = reverseList(next, prev, head);
-  return val ?? head;
+  return reverseList(next, head);
 };
