9cb2827046fd4bcd1f452bfe6cffc8fb12e4e568
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-25 20:52:33 -0400

    cleared out practice js

--- practice.js
+++
@@ -1,7 +0,0 @@
-var reverseList = function (head, next = null, prev = null) {
-  if (!head) return null;
-  next = head.next;
-  head.next = prev;
-  let val = reverseList(next, prev, head);
-  return val ?? head;
-};
