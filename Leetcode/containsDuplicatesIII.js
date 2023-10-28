fe8ead6bc7aeac3c666a89ce388ab14bcea80f8a
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-10-28 13:02:55 -0400

    moved ocntains duplicates to correct folder

--- Leetcode/containsDuplicatesIII.js
+++ Leetcode/containsDuplicatesIII.js
@@ -1,4 +1,31 @@
-var containsNearbyAlmostDuplicateBruteForce = function (nums, indexDiff, valueDiff) {
+var containsNearbyAlmostDuplicate = function (nums, indexDiff, valueDiff) {
+  let map = new Map();
+  let bucketSize = valueDiff + 1;
+  for (let i = 0; i < nums.length; i++) {
+    const mapKey = Math.floor(nums[i] / bucketSize);
+    if (map.has(mapKey)) return true;
+    if (map.has(mapKey - 1) && nums[i] - map.get(mapKey - 1) < bucketSize)
+      return true;
+    if (map.has(mapKey + 1) && map.get(mapKey + 1) - nums[i] < bucketSize)
+      return true;
+    map.set(mapKey, nums[i]);
+
+    if (i >= indexDiff) {
+      const oldMapKey = Math.floor(nums[i - indexDiff] / bucketSize);
+      map.delete(oldMapKey);
+    }
+  }
+
+  return false;
+};
+
+console.log(containsNearbyAlmostDuplicate([1, 2, 3, 1], 3, 0));
+
+var containsNearbyAlmostDuplicateBruteForce = function (
+  nums,
+  indexDiff,
+  valueDiff
+) {
   for (let i = 0; i < nums.length; i++) {
     let l = i - indexDiff;
     let r = indexDiff + i;
