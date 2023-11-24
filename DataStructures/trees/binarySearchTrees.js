2e28f5ff1055d61f3e1632e571a1d965a7061322
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-11-24 15:44:01 -0500

    made changes to binary search trees and solved leetcode problem reverse odd levels in practice.js

--- DataStructures/trees/binarySearchTrees.js
+++ DataStructures/trees/binarySearchTrees.js
@@ -14,6 +14,7 @@ class BTS {
   insert(val) {
     if (this.root) {
       let holder = this.root;
+      let conditon= true;
       while (true) {
         if (holder.val === val) {
           return undefined;
