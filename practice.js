d789816b0f5cdf7b7f49e33b69f2681fafba7325
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-05 11:51:39 -0500

    completed number of matches

---
+++ practice.js
@@ -0,0 +1,16 @@
+var numberOfMatches = function (n) {
+  let matchCount = 0;
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      matchCount += (n - 1) / 2;
+      n = Math.floor(n / 2) + 1;
+    } else {
+      matchCount += n / 2;
+      n = n / 2;
+    }
+  }
+
+  return matchCount;
+};
+
+console.log(numberOfMatches(14));
