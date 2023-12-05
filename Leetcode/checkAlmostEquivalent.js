b151d3c184ed1bf2622193e34c557a370e17ff18
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-05 12:34:13 -0500

    moved it to appropriate directory

--- Leetcode/checkAlmostEquivalent.js
+++ Leetcode/checkAlmostEquivalent.js
@@ -79,3 +79,25 @@ const checkAlmostEquivalentTwo = (word1, word2) => {
 };
 
 console.log(checkAlmostEquivalentTwo("abaaaccccc", "abcdeef"));
+
+
+const checkAlmostEquivalentIII = (word1, word2) => {
+  const frequencies = new Map();
+  for (let i of word1) {
+    frequencies.has(i)
+      ? frequencies.set(i, frequencies.get(i) + 1)
+      : frequencies.set(i, 1);
+  }
+  for (let i of word2) {
+    frequencies.has(i)
+      ? frequencies.set(i, frequencies.get(i) - 1)
+      : frequencies.set(i, -1);
+  }
+
+  for (let i of frequencies) {
+    if (Math.abs(frequencies.get(i[0])) > 3) return false;
+  }
+  return true;
+};
+
+console.log(checkAlmostEquivalentIII("abaaacccc", "abcdeef"));
