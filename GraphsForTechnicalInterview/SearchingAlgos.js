42e5cebdb9bf7a25abae39e7813439371c85e876
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-14 15:46:42 -0500

    implemented a bfs algo for graphs

--- GraphsForTechnicalInterview/SearchingAlgos.js
+++ GraphsForTechnicalInterview/SearchingAlgos.js
@@ -1,5 +1,5 @@
 const al = {
-  a: ["c","b"],
+  a: ["c", "b"],
   b: ["d"],
   c: ["e"],
   d: ["f"],
@@ -7,7 +7,8 @@ const al = {
   f: [],
 };
 
-const dft = (key, graph) => { // recursive
+const dft = (key, graph) => {
+  // recursive
   if (graph[key] === undefined) return [];
   const values = new Set();
   const helper = (key) => {
@@ -24,21 +25,36 @@ const dft = (key, graph) => { // recursive
 };
 console.log(dft("a", al));
 
-const dftIterative=(graph,source)=>{ // iterative depth first
-const stack=[source]
-const values=new Set([source])
-while(stack.length>0){
-    const current=stack.pop()
-    
-    for(let i of graph[current]){
-        values.add(i);
-        stack.push(i)
+const dftIterative = (graph, source) => {
+  // iterative depth first
+  const stack = [source];
+  const values = new Set([source]);
+  while (stack.length > 0) {
+    const current = stack.pop();
+
+    for (let i of graph[current]) {
+      values.add(i);
+      stack.push(i);
     }
-}
+  }
 
-    return [...values.values()]
+  return [...values.values()];
+};
 
-}
+console.log(dftIterative(al, "a"));
 
-console.log(dftIterative(al,"a"))
+const bfs = (key, graph) => { // breadth first search
+  const queue = [key];
+  const values = new Set([key]);
+  while (queue.length) {
+    let currentNode = queue.shift();
+    for (let i of graph[currentNode]) {
+      values.add(i)
+      queue.push(i)
 
+    }
+  }
+  return [...values.keys()]
+};
+
+console.log(bfs("a",al))
