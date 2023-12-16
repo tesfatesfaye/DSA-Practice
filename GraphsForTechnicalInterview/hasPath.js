7cc3f249cbca2ab0c5dad0b9ca9718ccccf421a5
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-16 15:49:02 -0500

    created a converter for Adjacency matrix to be adjacency list

--- GraphsForTechnicalInterview/hasPath.js
+++ GraphsForTechnicalInterview/hasPath.js
@@ -1,43 +1,50 @@
-const graph = { f: ["g", "i"], g: ["h"], h: [], i: ["g", "k"], j: ["i"],k:[] };
-
-const hasPath=(graph,src,dst,set=new Set())=>{
-    if(src===dst) return true
-    for(let neighbor of graph[src]){
-        if(!set.has(neighbor)){
-            set.add(neighbor)
-            if(hasPath(graph,neighbor,dst,set)===true) return true
-        }
-    }
-    return false
-}
-
-
-
-
-
-
-
-
-const hasPathTwo = (graph, src, dst) => {
- 
-  const passed = new Set();
-
-  const helper = (key) => {
-
-    if (!passed.has(key)) {
-      passed.add(key);
-      for (let i of graph[key]) {
-        if (i === dst) {
-          return true;
-        } else {
-          let newNode = helper(i);
-          if (newNode === true) return true;
-        }
-      }
-    }
-     return false
-  };
-  return helper(src)
+const graph = {
+  f: ["g", "i"],
+  g: ["h"],
+  h: [],
+  i: ["g", "k"],
+  j: ["i"],
+  k: [],
 };
 
-console.log(hasPath(graph, "j", "f"));
+const hasPath = (graph, src, dst, set = new Set()) => {
+  // recursive
+  if (src === dst) return true;
+  for (let neighbor of graph[src]) {
+    if (!set.has(neighbor)) {
+      set.add(neighbor);
+      if (hasPath(graph, neighbor, dst, set) === true) return true;
+    }
+  }
+  return false;
+};
+
+const hasPathBfs = (graph, src, dst) => {
+  const queue = [src];
+  const values = new Set(src);
+  while (queue.length) {
+    const current = queue.shift();
+    if (current === dst) return true;
+    for (let neighbor of graph[current]) {
+      if (!values.has(neighbor)) {
+        queue.push(neighbor);
+        values.add(neighbor);
+      }
+    }
+  }
+  return false;
+};
+
+const hasPathTwo = (graph, src, dst, set = new Set([src])) => {
+  if (src === dst) return true;
+  for (let neighbor of graph[src]) {
+    if (!set.has(neighbor)) {
+      set.add(neighbor)
+      if (hasPathTwo(graph, neighbor, dst, set)===true) return true;
+    }
+  }
+  return false
+};
+
+console.log(hasPathBfs(graph, "f", "k"));
+console.log(hasPathTwo(graph, "f", "j"));
---
+++ GraphsForTechnicalInterview/makeAdjcencyList.js
@@ -0,0 +1,18 @@
+const converter = (arr) => {
+  const graph = {};
+  for (let i of arr) {
+    for (let j of i) {
+      let values = i.filter((x) => x !== j);
+      if (graph[j]!==undefined) {
+        graph[j] = [...graph[j], ...values];
+      } else {
+        graph[j] = values;
+      }
+    }
+  }
+  return graph;
+};
+
+const edges=[['i','j'],['k','i'],['m','k'],['k','l'],['o','n']]
+const list=converter(edges)
+console.log(list)
