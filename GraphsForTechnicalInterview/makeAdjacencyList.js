a22fd8b1df6405a40fa3da0565b31cddb199c9ec
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-19 17:17:48 -0500

    cleaned up makeAdjacencyList

--- GraphsForTechnicalInterview/makeAdjacencyList.js
+++ GraphsForTechnicalInterview/makeAdjacencyList.js
@@ -1,20 +1,27 @@
 const converter = (arr) => {
   const graph = {};
-  for (let i of arr) {
-    for (let j of i) {
-      let values = i.filter((x) => x !== j);
-      if (graph[j]!==undefined) {
-        graph[j] = [...new Set([...values, ...graph[j]]).values()];
-      } else {
-
-        graph[j] = [...new Set(values).values()];
+  for (let edge of arr) {
+    for (let node of edge) {
+      if (graph[node] === undefined) graph[node] = new Set();
+      for (let value of edge) {
+        if (value !== node) {
+          graph[node].add(value);
+        }
       }
     }
   }
+  for (let node in graph) {
+    graph[node] = Array.from(graph[node]);
+  }
+
   return graph;
 };
 
-const edges=[['i','j','k'],['k','i'],['m','k'],['k','l'],['o','n']]
-const list=converter(edges)
-console.log(list)
-
+const edges = [
+  ["i", "j", "k"],
+  ["m", "k"],
+  ["k", "l"],
+  ["o", "n"],
+];
+const list = converter(edges);
+console.log(list);
