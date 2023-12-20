4373db6cb0b0cc8859217ef1870bcd095a89f93e
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-20 14:00:05 -0500

    implemented the largest component function without a helper method

--- GraphsForTechnicalInterview/largestComponent.js
+++ GraphsForTechnicalInterview/largestComponent.js
@@ -1,34 +1,55 @@
 //work in progress
 const graph = {
-  0: ["8", "1", "5"],
-  1: ["0"],
-  5: ["0", "8"],
-  8: ["0", "5"],
-  2: ["3", "4"],
-  3: ["2", "4"],
-  4: ["3", "2"],
+  1: ["2"],
+  2: ["1", "8"],
+  6: ["7"],
+  9: ["8"],
+  7: ["6", "8"],
+  8: ["9", "7", "2"],
 };
 
-const largestComponentHelper=(graph)=>{
-    let largestComponentSize=0
-    const visited=new Set()
-    const helper=(current,currentMax)=>{
-        if(!visited.has(current)){
-            visited.add(current)
-            currentMax++
-            for(let node of graph[current]){
-                currentMax=helper(node,currentMax)
-            }
-        }
-        return currentMax
-
+const largestComponentHelper = (graph) => {
+  let largestComponentSize = 0;
+  const visited = new Set();
+  const helper = (current, currentMax) => {
+    if (!visited.has(current)) {
+      visited.add(current);
+      currentMax++;
+      for (let node of graph[current]) {
+        currentMax = helper(node, currentMax);
+      }
     }
+    return currentMax;
+  };
 
-    for(let node in graph){
-        largestComponentSize=Math.max(largestComponentSize,helper(node,0))
-    }
+  for (let node in graph) {
+    largestComponentSize = Math.max(largestComponentSize, helper(node, 0));
+  }
 
-    return largestComponentSize
-}
+  return largestComponentSize;
+};
 
 console.log(largestComponentHelper(graph));
+
+const largestComponent = (
+  graph,
+  largest = 0,
+  node = null,
+  visited = new Set()
+) => {
+  if (node === null) {
+    for (let current in graph) {
+      largest = Math.max(largest, largestComponent(graph, 0, current, visited));
+    }
+  } else {
+    if (!visited.has(node)) {
+      largest++;
+      visited.add(node);
+      for (let current of graph[node]) {
+        largest = largestComponent(graph, largest, current, visited);
+      }
+    }
+  }
+  return largest
+};
+console.log(largestComponent(graph))
