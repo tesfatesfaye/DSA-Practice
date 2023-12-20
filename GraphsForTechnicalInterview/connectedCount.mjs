b742e0e5f4515676000823ae6684258380975126
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-12-20 00:11:54 -0500

    implemented connectedCount function

---
+++ GraphsForTechnicalInterview/connectedCount.mjs
@@ -0,0 +1,36 @@
+const helper = (val, graph, visited) => {
+  for (let node of graph.get(val)) {
+    if (visited.has(node)===false) {
+      visited.add(node);
+      helper(node, graph, visited);
+    }
+  }
+};
+
+const connectedCount = (graph) => {
+  let count = 0;
+  const visited = new Set();
+  for (let node of graph) {
+    if (!visited.has(node[0])) {
+      visited.add(node[0]);
+      helper(node[0], graph, visited);
+      count++;
+      console.log(visited.values())
+    }
+  }
+
+  return count;
+};
+
+const graphMap = new Map([
+  [3, []],
+  [4, [6]],
+  [6, [4, 5, 7, 8]],
+  [8, [6]],
+  [7, [6]],
+  [5, [6]],
+  [1, [2]],
+  [2, [1]],
+]);
+
+console.log(connectedCount(graphMap));
